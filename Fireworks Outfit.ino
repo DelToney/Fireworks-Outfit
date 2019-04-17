602fc99167eedb9b4dd7b1bdf36f0e4e3fb7c0a8
Delton Toney <deltinator@gmail.com>
2019-04-16 23:08:00 -0400

    Refined, Used Delay

--- Fireworks Outfit.ino
+++ Fireworks Outfit.ino
@@ -1,100 +1,134 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
 
-#include <FastLED.h>
-#define NUM_LEDS 8
-#define LED_STRING_1 10
-#define LED_STRING_2 6
-#define LED_STRING_3 7
+#define PIN 6
 
-#define DELAY 10
-#define FADED_INTENSITY 2
-#define FADED_DELAY 25
-#define DELAY_START 10
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
 
-CRGB led_string1[NUM_LEDS];
-//CRGB led_string2[NUM_LEDS];
-//CRGB led_string3[NUM_LEDS];
-boolean dimOrLight[NUM_LEDS];
-int nextBoom[NUM_LEDS];
-int startingColors[3];
-int waveDelay = 50;
-int startDelay = 100;
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
 
 void setup() {
-  // put your setup code here, to run once:
-  FastLED.addLeds<NEOPIXEL, LED_STRING_1>(led_string1, NUM_LEDS);
-  //  FastLED.addLeds<NEOPIXEL, LED_STRING_2>(led_string2, NUM_LEDS);
-  //  FastLED.addLeds<NEOPIXEL, LED_STRING_3>(led_string3, NUM_LEDS);
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  // End of trinket special code
 
-  for (int i = 0; i < NUM_LEDS; i++) {
-    nextBoom[i] = FADED_DELAY + (i * 30);
-  }
-
-  startingColors[0] = random8(255);
-  startingColors[1] = random8(255);
-  startingColors[2] = random8(255);
-
-
-  for (int i = 0; i < NUM_LEDS; i++) {
-    led_string1[i].setRGB( startingColors[0] - (10 * i), startingColors[1] - (10 * i), startingColors[2] - (10 * i));
-    //    led_string2[i].setRGB( 185, 185, 185);
-    //    led_string3[i].setRGB( 185, 185, 185);
-  }
+  strip.begin();
+  strip.setBrightness(50);
+  strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //  led_string1, led_string2, led_string3 |= MIN_BRIGHTNESS;
+  // Some example procedures showing how to display to the pixels:
+  colorWipe(strip.Color(255, 0, 0), 50); // Red
+  colorWipe(strip.Color(0, 255, 0), 50); // Green
+  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+//colorWipe(strip.Color(0, 0, 0, 255), 50); // White RGBW
+  // Send a theater pixel chase in...
+  theaterChase(strip.Color(127, 127, 127), 50); // White
+  theaterChase(strip.Color(127, 0, 0), 50); // Red
+  theaterChase(strip.Color(0, 0, 127), 50); // Blue
 
-  for (int current_led = 0; current_led < NUM_LEDS; current_led++) {
-    //LED string 1
-    led_string1[current_led].fadeToBlackBy(FADED_INTENSITY);
-    //    if (led_string1[current_led] >= CRGB(DELAY_START, DELAY_START, DELAY_START)) {
-    Serial.println(nextBoom[current_led]);
-    //    } else { // assume led is below x brightness
+  rainbow(20);
+  rainbowCycle(20);
+  theaterChaseRainbow(50);
+}
 
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
 
-    /*if ((led_string1[NUM_LEDS - 1] && !led_string1[0])|(led_string1[NUM_LEDS - 1] && led_string1[0])) {//dont do unless both first and last led are off
-    } else*/ if (nextBoom[current_led] <= millis()) {//set light if previous timer is 
-      led_string1[current_led].setRGB (startingColors[0], startingColors[1], startingColors[2]);
-      nextBoom[current_led+1] = millis()+waveDelay;
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
     }
-
-
-
-
-
-
-
-    //      if (!led_string1[NUM_LEDS - 1] && current_led == 0) {
-    //        nextBoom[current_led] =;
-    //      } else if (nextBoom[current_led] <= millis() + FADED_DELAY &&  nextBoom[0] == FADED_DELAY) {
-    //        led_string1[current_led].setRGB (startingColors[0], startingColors[1], startingColors[2]);
-    //        nextBoom[current_led] = millis();
-    //      }
-    if (current_led >= 1 && current_led < 7 ) {
-    led_string1[current_led] = led_string1[1];
-    nextBoom[current_led] = nextBoom[1];
+    strip.show();
+    delay(wait);
   }
-  }
-  //    if (nextBoom[0] < millis()-startDelay) {
-  //        led_string1[0].setRGB (startingColors[0], startingColors[1], startingColors[2]);
-  //      }
-
-  
-  //  }
-  if (!led_string1[NUM_LEDS - 1] && !led_string1[0]) {
-    nextBoom[0] = millis()+startDelay;
-  }
-  FastLED.show();
-
-
-  delay(DELAY + random8(0, 9));
-
 }
 
-CRGB Dim (int DimBy, CRGB led) {
-  led.setRGB (led.r - DimBy, led.g - DimBy, led.b - DimBy);
-  return led;
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
 }
 
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
 
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
